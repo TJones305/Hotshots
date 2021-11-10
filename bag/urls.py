b9ad17c9af543adaa4a53133dc34d069e2cd7c6a
TJones305 <tim.w.j@hotmail.com>
2021-11-10 11:34:58 +0000

    Added view to update basket items

--- bag/urls.py
+++ bag/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
+    path('modify/<item_id>/', views.modify_bag, name='modify_bag'),
+    path('remove/<item_id>/', views.remove_bag_item, name='remove_bag_item'),
+
+
 ]
