e0c5b88674a8561152674d3254e61f86ae0e21a7
TJones305 <tim.w.j@hotmail.com>
2021-11-11 21:25:35 +0000

    Added user notifications including css, basket preview and additional messages

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
@@ -12,6 +15,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """Add quantity of a product to shopping basket """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -23,15 +27,25 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
+                messages.info(request,
+                              f'Updated {product.name} {size.upper()} Qty to {bag[item_id]["items_by_size"][size]}')
             else:
                 bag[item_id]['items_by_size'][size] = quantity
+                messages.success(request,
+                                 f'{product.name} {size.upper()} added to basket')
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
+            messages.success(request,
+                             f'{product.name} {size.upper()} added to basket')
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
+            messages.info(request,
+                          f'Updated {product.name} Qty to {bag[item_id]}')
         else:
             bag[item_id] = quantity
+            messages.success(request,
+                             f'{product.name} added to basket')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -40,6 +54,7 @@ def add_to_bag(request, item_id):
 def modify_bag(request, item_id):
     """ Modify amount of an item & display price """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     size = None
     if 'product_size' in request.POST:
@@ -49,15 +64,23 @@ def modify_bag(request, item_id):
     if size:
         if quantity > 0:
             bag[item_id]['items_by_size'][size] = quantity
+            messages.info(request,
+                          f'Updated {product.name} {size.upper()} Qty to {bag[item_id]["items_by_size"][size]}')
         else:
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
+                messages.warning(request,
+                                 f'{product.name} {size.upper()} removed from basket')
     else:
         if quantity > 0:
             bag[item_id] = quantity
+            messages.info(request,
+                          f'Updated {product.name} Qty to {bag[item_id]}')
         else:
             bag.pop(item_id)
+            messages.warning(request,
+                             f'{product.name} Removed from basket')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -67,6 +90,7 @@ def remove_bag_item(request, item_id):
     """ Remove item from basket """
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         size = None
         if 'product_size' in request.POST:
             size = request.POST['product_size']
@@ -76,11 +100,16 @@ def remove_bag_item(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
+                messages.warning(request,
+                                 f'{product.name} {size.upper()} removed from basket')
         else:
             bag.pop(item_id)
+            messages.warning(request,
+                             f'{product.name} Removed from basket')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error occured: {e}')
         return HttpResponse(status=500)
