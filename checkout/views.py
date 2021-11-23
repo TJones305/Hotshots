6779d888d4d4e1db8f4c27a2aed57081e05caadd
TJones305 <tim.w.j@hotmail.com>
2021-11-23 21:43:25 +0000

    Added checkout form data caching in payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,8 @@
 import stripe
+import json
 
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -8,6 +10,22 @@ from products.models import Product
 from bag.contexts import bag_contents
 from .forms import OrderForm
 from .models import Order, OrderLineItem
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag',{})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request,'Apologies, payment is unable to be \
+        recieved currently. Please try again later.')
 
 
 def checkout(request):
