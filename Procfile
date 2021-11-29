5b8ac1f63800b1f11bedbe12cee33927ddae3e05
TJones305 <tim.w.j@hotmail.com>
2021-11-29 19:51:07 +0000

    pre heroku deployment setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn hotshots.wsgi:application
--- hotshots/settings.py
+++ hotshots/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$g6k=rnvv0pl-jfj5#i!4_-9u-75#9qjpu$9o!x-u^b*zwmw=n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hotshots.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +122,17 @@ WSGI_APPLICATION = 'hotshots.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-  'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-  }
- }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.envrion.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
